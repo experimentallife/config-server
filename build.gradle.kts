806bb93ca98931798621a8e3c08af4aaf5722ba8
experimentallife <user@experimentallife.dev>
2022-08-04 05:00:42 -0500

    :construction: in progress

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.*
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
 	id("org.springframework.boot") version "2.7.2"
@@ -40,4 +42,12 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	testLogging {
+		events(STARTED, SKIPPED, PASSED, FAILED)
+		exceptionFormat = FULL
+		showStandardStreams = true
+		showExceptions = true
+        showCauses = true
+        showStackTraces = true
+	}
 }
